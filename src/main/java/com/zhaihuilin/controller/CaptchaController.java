05b4819445f23f920f912ac8355a18dae187b492
zhai926 <17317900836@163.com>
2018-02-05 15:36:49 +0800

    添加注解

--- src/main/java/com/zhaihuilin/controller/CaptchaController.java
+++ src/main/java/com/zhaihuilin/controller/CaptchaController.java
@@ -3,6 +3,7 @@ package com.zhaihuilin.controller;
 import com.google.code.kaptcha.Producer;
 import com.zhaihuilin.utils.JedisUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -17,6 +18,7 @@ import java.awt.image.BufferedImage;
  * 验证码请求
  * Created by zhaihuilin on 2018/2/1  14:42.
  */
+@Controller
 public class CaptchaController {
 
     @Autowired
